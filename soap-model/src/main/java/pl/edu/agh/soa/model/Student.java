1e01950681ac8f5f1be718f22ed0649501529399
lgosek <luki2012@gmail.com>
2019-05-28 20:14:13 +0200

    Added Student Json property names

--- soap-model/src/main/java/pl/edu/agh/soa/model/Student.java
+++ soap-model/src/main/java/pl/edu/agh/soa/model/Student.java
@@ -1,13 +1,17 @@
 package pl.edu.agh.soa.model;
 
-import javax.annotation.PostConstruct;
-import javax.ejb.Singleton;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import javax.validation.constraints.Min;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElementWrapper;
 import java.util.LinkedList;
 import java.util.List;
 
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Student {
     private int Id;
     private String FirstName;
@@ -17,7 +21,7 @@ public class Student {
     private int StudentNumber;
 
     private List<String> Courses;
-    private String avatarFilename;
+    private String AvatarFilename;
 
     public Student(int id, String name, String lastName, int num){
         this.Id = id;
@@ -27,7 +31,7 @@ public class Student {
         this.Courses = new LinkedList<>();
         this.Courses.add("Kurs1");
         this.Courses.add("Kurs2");
-        this.avatarFilename = "/home/lukasz/Dokumenty/SOA/backup/soap/soap-api/src/main/resources/img.jpg";
+        this.AvatarFilename = "/home/lukasz/Dokumenty/SOA/backup/soap/soap-api/src/main/resources/img.jpg";
     }
 
     public Student(){
@@ -43,7 +47,7 @@ public class Student {
     }
 
     public void setAvatarFilename (String avatarFilename) {
-        this.avatarFilename = avatarFilename;
+        this.AvatarFilename = avatarFilename;
     }
 
     public void setFirstName (String FirstName) {
@@ -71,29 +75,35 @@ public class Student {
         return -1;
     }
 
+    @JsonProperty("Id")
     public int getId () {
         return Id;
     }
 
+    @JsonProperty("FirstName")
     public String getFirstName () {
         return FirstName;
     }
 
+    @JsonProperty("LastName")
     public String getLastName () {
         return LastName;
     }
 
+    @JsonProperty("StudentNumber")
     public int getStudentNumber () {
         return StudentNumber;
     }
 
     @XmlElementWrapper(name = "Courses")
     @XmlElement(name = "CourseName")
+    @JsonProperty("Courses")
     public List<String> getCourses () {
         return Courses;
     }
 
+    @JsonProperty("AvatarFilename")
     public String getAvatarFilename () {
-        return avatarFilename;
+        return AvatarFilename;
     }
 }
